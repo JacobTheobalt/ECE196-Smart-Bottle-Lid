e413ab8a37923464dde344430084608db313072f
JacobTheobalt <jtheobal@ucsd.edu>
2023-05-17 17:37:27 -0700

    added Arduino code v1.0

---
+++ ece196_lidSampling/Peripherals.ino
@@ -0,0 +1,44 @@
+  #define MIN_LIGHT 20
+  
+  float getWaterLevel()
+  {
+    uint8_t NewDataReady = 0;
+    VL53L4CD_Result_t results;
+    uint8_t status;
+    char report[64];
+    
+    do {
+      status = sensor_vl53l4cd_sat.VL53L4CD_CheckForDataReady(&NewDataReady);
+    } while (!NewDataReady);
+
+    if ((!status) && (NewDataReady != 0)) {
+      // (Mandatory) Clear HW interrupt to restart measurements
+      sensor_vl53l4cd_sat.VL53L4CD_ClearInterrupt();
+
+      // Read measured distance. RangeStatus = 0 means valid data
+      sensor_vl53l4cd_sat.VL53L4CD_GetResult(&results);
+      snprintf(report, sizeof(report), "Status = %3u, Distance = %5u mm, Signal = %6u kcps/spad\r\n",
+              results.range_status,
+              results.distance_mm,
+              results.signal_per_spad_kcps);
+    }
+      return results.distance_mm; //return the range read in mm
+  }
+
+  bool checkLid()
+  {
+    light_level = analogRead(pdiodePin)
+    if(light_level > MIN_LIGHT)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  bool checkStability()
+  {
+    //insert code here for stability checking using the IMU
+  }
---
+++ ece196_lidSampling/ece196_lidSampling.ino
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53l4cd_class.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define DEV_I2C Wire
+#define SerialPort Serial
+
+// Components.
+VL53L4CD sensor_vl53l4cd_sat(&DEV_I2C, A1);
+
+unsigned long current_time = 0;
+unsigned long start_time = millis();
+int samplePin = D1;
+int pdiodePin = A0;
+/* Setup ---------------------------------------------------------------------*/
+
+void setup()
+{
+  // Initialize serial for output.
+  SerialPort.begin(115200);
+  // Initialize I2C bus.
+  DEV_I2C.begin();
+  // Configure VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.begin();
+  // Switch off VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.VL53L4CD_Off();
+  //Initialize VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.InitSensor();
+  // Program the highest possible TimingBudget, without enabling the
+  // low power mode. This should give the best accuracy
+  sensor_vl53l4cd_sat.VL53L4CD_SetRangeTiming(200, 0);
+  // Start Measurements
+  sensor_vl53l4cd_sat.VL53L4CD_StartRanging();
+  pinMode(samplePin, OUTPUT);
+  digitalWrite(samplePin, HIGH);
+  pinMode(pdiodePin, INPUT);
+}
+
+void loop()
+{
+  if(current_time - start_time > 2000 && checkLid() && checkStability())
+  {
+    Serial.println(getWaterLevel());
+    start_time = millis();
+  }
+  current_time = millis();
+}
